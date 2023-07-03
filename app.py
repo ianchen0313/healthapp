20f79511106bafc79ccbc1694187dd013498bff0
ianchen0313 <129363423+ianchen0313@users.noreply.github.com>
2023-07-03 22:51:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ import streamlit as st
 import pickle
 import pandas as pd
 import sklearn
-import time 
+import time
 from model import final_model
 from PIL import Image
 
-img_lis = []
+img_lis_info = []
 for i in range(1,9):
     img = Image.open(f"投影片{i}.jpg")
-    img_lis.append(img)
+    img_lis_info.append(img)
+
+img_lis_teach = []
+for i in range(1,8):
+    img = Image.open(f"衛教_{i}.jpg")
+    img_lis_teach.append(img)
 
 model = final_model
 
@@ -62,33 +67,8 @@ def predict():
      
 def health_info():
     st.warning('衛教資訊(有效預防與延緩慢性腎臟病)')
-    st.text(
-    """
-    1.運動：
-    (1)有氧運動：一周3~5天，一天20~60分鐘，如游泳、騎腳踏車、走路
-    (2)阻力運動：一周2~3天，一組10~15反覆，要練習到8~10種肌群，如彈力帶、機器
-    
-    2.飲食：
-    (1)注意鈉、蛋白、脂肪含量。選擇低飽和脂肪，反式脂肪，膽固醇，鹽(鈉)和添加糖的食物
-    (2)避免醃漬類食品、避免罐頭、冷凍食品等加工食品
-    (3)不吃高鹽高脂速食如漢堡、炸雞、鹹酥雞、披薩、泡麵、火鍋、滷味
-    (4)每天飲食限鹽少於2,300毫克
-    (5)限蛋白飲食
-    (6)低鉀、低磷飲食(大部分包裝食品或飲料都含有磷)
-    
-    *推薦甜品選擇：愛玉、仙草、粉粿等食用前添加少量蜂蜜；果凍、粉圓、西谷米、涼圓
-    
-    *推薦食物選擇：炒冬粉、水晶餃、地瓜
-    
-    *小心高磷食物：麵筋、毛豆、蠶豆、花生、杏仁、巧克力、碳酸飲料、奶製品
-    
-    *小心高鉀食物：低鈉或薄鹽醬油 (因減少的鈉會由鉀代替，鉀易過高)、高湯、果汁、香蕉、香瓜
-                 榴槤，奇異果、草莓、柑橘、咖啡、茶、運動飲料、堅果類、巧克力、梅子汁、蕃茄醬
-                 九層塔、木耳、菠菜、空心菜、海帶
-    
-    *小心高鈉食物：各種醃製食品，如：醬菜、豆腐乳、肉鬆、麵線、鹽、味增、沙茶醬、烏醋、蕃茄醬
-                 豆瓣醬等調味料，蘇打餅乾、培根、熱狗、香腸、芹菜、菠菜、罐頭食品、汽水
-    """)
+    for img in img_lis_teach:
+        st.image(img)
 
 def hospital(region):
     lookup_table = {
@@ -142,7 +122,7 @@ def hospital(region):
 
 
 # app section
-st.title('成功逆轉腎 :point_up:')
+st.title('問卷快篩')
 st.caption('本預測模型是由「腎敗難免」團隊利用衛生福利部國民健康署 \
            (前身：衛生署國民健康局) 於民國 91 年所施行的「台灣地區國民健康促進知識、態度與行為調查」作為原始資料進行機器學習訓練而得\
             ，預測結果僅為參考仍應以醫師診斷結果為主，模型表現數據如下：')
@@ -159,12 +139,12 @@ with st.expander("查看更多關於模型表現"):
 
 
 with (st.expander("查看更多認識慢性腎臟病")):
-    for img in img_lis:
+    for img in img_lis_info:
         st.image(img)
 
 st.write("---")
 
-st.markdown('#### 以下共有 23 題，請依實際狀況回答')
+st.markdown('#### 以下共有 22 題，請依實際狀況回答')
 st.markdown('##### 基本資料 (身高體重是為了算 bmi)')
 
 region = st.selectbox('您目前居住在哪裡',
@@ -173,8 +153,8 @@ region = st.selectbox('您目前居住在哪裡',
                        '宜蘭縣','花蓮縣','台東縣','金門縣','澎湖縣','連江縣'])
 age = st.number_input('您目前的年齡',
                       min_value=1,max_value=100,value=25)
-height = st.number_input('您目前身高(cm)',min_value=100, max_value=200, value=165)
-weight = st.number_input('您目前體重(kg)',min_value=1, max_value=200, value=60)
+height = st.number_input('您目前身高(cm)',min_value=1,max_value=200,value=165)
+weight = st.number_input('您目前體重(kg)',min_value=1,max_value=200,value=60)
 bmi = weight / ((height/100)**2)
 compare = st.selectbox('和去年比較之健康狀況',
                        ['比去年差很多', '比去年差一些', '和去年持平','比去年好一些','比去年好很多'],index=2)
